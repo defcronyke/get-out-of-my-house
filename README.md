f9662cdd30f332ff8849caf89ac53397c5089765
Jeremy Carter <jeremy@jeremycarter.ca>
2013-10-03 19:36:45 -0400

    Added license, main file, and CMakeLists.txt
    Updated README.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-get-out-of-my-house
-===================
+Get Out Of My House!
+-<|>=(_)+{}+[_]=<|>-
 
 A classic-style RPG.
+=------------------=
+This game is in the early stages of development, the first release is still a
+long way off.
---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+/* Ⓒ Copyright 2013 Jeremy Carter
+ *
+ * This file is part of get-out-of-my-house.
+ *
+ * get-out-of-my-house is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published 
+ * by the Free Software Foundation.
+ *
+ * get-out-of-my-house is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with get-out-of-my-house. If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
+ *
+ * src/main.cpp
+ *
+ * Created on: Oct 3, 2013
+ *     Author: Jeremy Carter
+ *      Email: Jeremy [at] JeremyCarter [dot] ca
+ */
+
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+	std::cout << "\nLoading Get Out Of My House!...\n\n"
+			  << "Ⓒ Copyright 2013 Jeremy Carter. Licensed under the GNU GPL version 3.\n" << std::endl;
+
+	return 0;
+}
